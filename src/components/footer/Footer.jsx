2634ee42cb2872d4bc41ce6023713ee27be0c005
Mohit <mohitgadhavi1@gmail.com>
2023-06-14 00:41:12 +0530

    made small changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,34 +7,24 @@ const Footer = () => {
     <div className={styles.container}>
       <div>©2023 Lamamia. All rights reserved.</div>
       <div className={styles.social}>
-        <Image
-          src="/1.png"
-          width={15}
-          height={15}
-          className={styles.icon}
-          alt="Lama Dev Facebook Account"
-        />
-        <Image
-          src="/2.png"
-          width={15}
-          height={15}
-          className={styles.icon}
-          alt="Lama Dev"
-        />
-        <Image
-          src="/3.png"
-          width={15}
-          height={15}
-          className={styles.icon}
-          alt="Lama Dev"
-        />
-        <Image
-          src="/4.png"
-          width={15}
-          height={15}
-          className={styles.icon}
-          alt="Lama Dev"
-        />
+        <a>
+          <Image
+            src="/icon_github.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="Lama Dev"
+          />
+        </a>
+        <a>
+          <Image
+            src="/4.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="Lama Dev"
+          />
+        </a>
       </div>
     </div>
   );
