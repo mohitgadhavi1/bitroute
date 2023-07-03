645bc42f9b41f246cfedb5ff666e77a325b5d7c8
Mohit <mohitgadhavi1@gmail.com>
2023-07-03 20:59:57 +0530

    subscriber form added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,42 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/robot_bgremove.png";
 import Button from "@/components/Button/Button";
 import Head from "next/head";
+import Popup from "@/components/Popup";
+import { useState } from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+  console.log(isOpen);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+
+    if (isValidEmail) {
+      // Perform your subscription logic here
+      console.log("Subscribed with email:", email);
+      togglePopup();
+    } else {
+      setIsValid(false);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setIsValid(true);
+  };
+
   return (
     <>
       <div
@@ -29,7 +61,76 @@ export default function Home() {
             Landscape. Stay Ahead with Cutting-Edge Insights and Updates on
             Artificial Intelligence Research.
           </p>
-          <Button url="/articles" text="Subscribe" />
+          <button
+            className="
+          cursor-pointer
+          bg-[#53c28b]
+          rounded-md
+          border: none;
+          w-max
+          text-white
+          px-6
+          py-4
+          hover:bg-[#53c28b]/50
+          
+          "
+            onClick={togglePopup}
+          >
+            Subscribe
+          </button>
+
+          <Popup isOpen={isOpen} onClose={togglePopup}>
+            <h1 className="text-gray-600 text-4xl font-semibold mb-2">
+              Subscribe to our Newsletter
+            </h1>
+            <p className="text-gray-500 mb-6">
+              Receive Valuable Articles and Resources Delivered to Your Inbox
+              Every Week
+            </p>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-600  text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email Address
+                </label>
+                <input
+                  className={`border ${
+                    isValid ? "border-gray-300" : "border-red-500"
+                  } rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-green-500`}
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+                {!isValid && (
+                  <p className="text-red-500 text-xs mt-1">
+                    Please enter a valid email address.
+                  </p>
+                )}
+              </div>
+
+              <button
+                className="cursor-pointer
+                bg-[#53c28b]
+                rounded-md
+                border: none;
+                w-max
+                text-white
+                px-6
+                py-2
+                hover:bg-[#53c28b]/50"
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+            <p className="mt-4 text-sm text-gray-400">
+              * No Spam, Just Informative Emails Focused on Education
+            </p>
+          </Popup>
         </div>
       </div>
     </>
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -6,11 +6,11 @@ import { AiOutlineClose } from "react-icons/ai";
 const Popup = ({ isOpen, onClose, children }) => {
   return (
     <div
-      className={`fixed inset-0   bg-opacity-50 flex items-center justify-center ${
+      className={`fixed inset-0  bg-opacity-50 flex items-center justify-center ${
         isOpen ? "" : "hidden"
       }`}
     >
-      <div className="bg-white relative min-w-[50vw] min-h-[50vh]  p-6 rounded-lg shadow-lg">
+      <div className="bg-white  relative min-w-[50vw] min-h-[50vh] max-w-[90vw] max-h-[90vh]  p-6 rounded-lg shadow-lg">
         <button
           className="absolute top-4 right-4 text-black hover:text-gray-700"
           onClick={onClose}
